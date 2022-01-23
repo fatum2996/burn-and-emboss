68abe847c321b7c4aad87ed8cf0745f3181fc348
fatum <fa-tum@yandex.ru>
2022-01-23 19:24:51 +0300

    Deleted debugging signals

--- bae.ino
+++ bae.ino
@@ -41,7 +41,6 @@ void callback(){ //обаботка пунктира
 }
  
 void setup() {  
-  Serial.begin(9600); //отладка
   InitTimersSafe();
   pinConfig(); //настройка выводов
   display.begin(); //включение экрана
@@ -57,7 +56,6 @@ void setup() {
   systemData.heaterState = HEATER_STATE_OFF;
   systemData.heaterTemperatureState = HEATER_TEMPERATURE_LOW;
   systemData.heaterTargetTemperature = heaterTargetTemperature;  
-//  Serial.println("Hello"); //отладка
   maxTimer.start(); //таймер опроса датчика температуры
   delay(500); //задержка для включения датчика
   interrupts(); //разрешение прерываний
@@ -68,8 +66,6 @@ void loop() {
   //чтение температуры
   if(maxTimer.isReady()) {
     reading = readMAX();
-  //  Serial.println("reading");
- //   Serial.println(reading);
     if((reading & 0x8000)) //общая ошибка
       errorState = HEATER_STATE_ERROR_UNKNOWN;
     else {
@@ -91,22 +87,15 @@ void loop() {
       if(screenState){
         if (systemData.heaterTemperature <= systemData.heaterTargetTemperature - lowerDelta) { //если температура меньше чем уставка минус допуск
           systemData.heaterTemperatureState = HEATER_TEMPERATURE_LOW;//включить нагреватель
-  //      Serial.println("Heater on");
           digitalWrite(PIN_HEATER, HIGH);
           systemData.heaterState = HEATER_STATE_ON;
         } else if (systemData.heaterTemperature >= systemData.heaterTargetTemperature + higherDelta){ //если температура больше чем уставка плюс допуск
           systemData.heaterTemperatureState = HEATER_TEMPERATURE_HIGH;//выключить нагреватель
           digitalWrite(PIN_HEATER, LOW);
-  //     Serial.println("Heater off");
           systemData.heaterState = HEATER_STATE_OFF;
         } else {//если температура в диапазоне
           systemData.heaterTemperatureState = HEATER_TEMPERATURE_IN_RANGE;    //выключить нагреватель    
-      //  digitalWrite(PIN_HEATER, LOW);
-  //     Serial.println("Heater off");
-       //   systemData.heaterState = HEATER_STATE_IDLE;
         }
- //    Serial.println("t"); //отладка   
- //   Serial.println(systemData.heaterTemperature); //отладка
       }
     }
   }
@@ -117,7 +106,6 @@ void loop() {
   if((digitalRead(PIN_24V)==LOW)&&(timerBusy == 0)) { //если пришел сигнал 24 вольта
     pneumoTimer.setTimeout(pneumaticReactionDelay);   //запускаем таймер задержки
     timerBusy = 1;
-//    Serial.println("24V!");  //отладка
   }
   
   if(digitalRead(PIN_24V)){ //если сигнала нет
@@ -127,19 +115,15 @@ void loop() {
   }
   if(pneumoTimer.isReady()) { //задержка закончилась
     digitalWrite(PIN_PNEUMO, HIGH);  //включаем пневмо
-//    Serial.println("Pneumo!");   //отладка
   }
 //обработка кнопки L+
   if(buttonLPlus.update()) {
     if(buttonLPlus.read() == 0) {//кнопка нажата
-//      Serial.println("Button LP pressed"); //отладка
       if(buttonTimer.isEnabled() == 0) {
         buttonTimer.setTimeout(buttonHoldDelay);   //запускаем таймер удержания
-  //      Serial.println("Hold timer started");//отладка
         buttonLPlusDown = 1; //фиксируем, что кнопка была нажата
       }
     } else { //кнопка отпущена
-  //    Serial.println("Button LP released");
       buttonTimer.stop(); //останавливаем таймер нажатия и сбрасываем флаг нажатия 
       smallButtonTimer.stop();
       systemData.laserBeamDutyPercent += laserBeamDutyStep; //увеличиваем бим пауэр
@@ -148,17 +132,14 @@ void loop() {
       }
       buttonLPlusDown = 0;
     }
-  }
+  }  
   if(buttonLPlusDown) { //
     if(buttonTimer.isReady()){
- //     Serial.println("Hold timer ready");  //отладка
       if( smallButtonTimer.isEnabled() == 0 ) {//запускаем короткий таймер, каждое срабатывание этого таймера при удержании кнопки - приращение значения
         smallButtonTimer.setInterval(smallButtonTimerDelay);      
-  //      Serial.println("small started");   //отладка  
       }
     }
     if(smallButtonTimer.isReady()){
- //     Serial.println("small ready");   
       systemData.laserBeamDutyPercent += laserBeamDutyStep; //приращение значения при удержании кнопки
       if(systemData.laserBeamDutyPercent > 100) {
         systemData.laserBeamDutyPercent = 100;
@@ -168,14 +149,11 @@ void loop() {
 //обработка кнопки L-
   if(buttonLMinus.update()) {
     if(buttonLMinus.read() == 0) {//кнопка нажата
-   //   Serial.println("Button LP pressed");
       if(buttonTimer.isEnabled() == 0) {
         buttonTimer.setTimeout(buttonHoldDelay);   
-   //     Serial.println("Hold timer started");
         buttonLMinusDown = 1;
       }
     } else { //кнопка отпущена
- //     Serial.println("Button LP released");
       buttonTimer.stop();
       smallButtonTimer.stop();
       systemData.laserBeamDutyPercent -= laserBeamDutyStep; 
@@ -187,14 +165,11 @@ void loop() {
   }
   if(buttonLMinusDown) {
     if(buttonTimer.isReady()){
- //     Serial.println("Hold timer ready");  
       if( smallButtonTimer.isEnabled() == 0 ) {
         smallButtonTimer.setInterval(smallButtonTimerDelay);      
-  //      Serial.println("small started");   
       }
     }
     if(smallButtonTimer.isReady()){
- //     Serial.println("small ready");   
       systemData.laserBeamDutyPercent -= laserBeamDutyStep; 
       if(systemData.laserBeamDutyPercent < 1) {
         systemData.laserBeamDutyPercent = 1;
@@ -204,9 +179,7 @@ void loop() {
 //обработка кнопки L Reset
   if(buttonLReset.update()) {
     if(buttonLReset.read() == 0) {//кнопка нажата
- //     Serial.println("Button L Reset pressed");
     } else { //кнопка отпущена
-//      Serial.println("Button L Reset released");
       systemData.laserBeamDutyPercent = laserBeamDutyDefault; 
       systemData.patternPresetNo = 0; //меняем пресет
       systemData.patternFrequency = pwmPatternPreset[systemData.patternPresetNo].frequency;
@@ -217,16 +190,13 @@ void loop() {
   if(buttonTMinus.update()) {
     if(buttonTMinus.read() == 0) {//кнопка нажата
         buttonTMinusDown1 = 1;
- //     Serial.println("Button LP pressed");
       if(buttonTimer.isEnabled() == 0) {
         buttonTimer.setTimeout(buttonHoldDelay);   
-   //     Serial.println("Hold timer started");
         buttonTMinusDown = 1;
-        }      
+      }      
     } else { //кнопка отпущена
 
-        buttonTMinusDown1 = 0; 
-  //    Serial.println("Button LP released");
+      buttonTMinusDown1 = 0; 
       buttonTimer.stop();
       smallButtonTimer.stop();
       systemData.heaterTargetTemperature -= heaterTargetTemperatureStep; 
@@ -239,14 +209,11 @@ void loop() {
  
   if(buttonTMinusDown) {
     if(buttonTimer.isReady()){
-   //   Serial.println("Hold timer ready");  
       if( smallButtonTimer.isEnabled() == 0 ) {
         smallButtonTimer.setInterval(smallButtonTimerDelay);      
-    //    Serial.println("small started");   
       }
     }
     if(smallButtonTimer.isReady()){
-  //    Serial.println("small ready");   
       systemData.heaterTargetTemperature -= heaterTargetTemperatureStep; 
       if(systemData.heaterTargetTemperature < heaterTargetTemperatureMin) {
         systemData.heaterTargetTemperature = heaterTargetTemperatureMin;
@@ -256,15 +223,12 @@ void loop() {
 //обработка кнопки T+
   if(buttonTPlus.update()) {
     if(buttonTPlus.read() == 0) {//кнопка нажата
-  //    Serial.println("Button LP pressed");
       buttonTPlusDown1 = 1;
       if(buttonTimer.isEnabled() == 0) {
         buttonTimer.setTimeout(buttonHoldDelay);   
-  //      Serial.println("Hold timer started");
         buttonTPlusDown = 1;
       }
     } else { //кнопка отпущена
- //     Serial.println("Button LP released");
       buttonTimer.stop();
       smallButtonTimer.stop();
       buttonTPlusDown1 = 0;
@@ -277,14 +241,11 @@ void loop() {
   }
   if(buttonTPlusDown) {
     if(buttonTimer.isReady()){
- //     Serial.println("Hold timer ready");  
       if( smallButtonTimer.isEnabled() == 0 ) {
         smallButtonTimer.setInterval(smallButtonTimerDelay);      
-   //     Serial.println("small started");   
       }
     }
     if(smallButtonTimer.isReady()){
-   //   Serial.println("small ready");   
       systemData.heaterTargetTemperature += heaterTargetTemperatureStep; 
       if(systemData.heaterTargetTemperature > heaterTargetTemperatureMax) {
         systemData.heaterTargetTemperature = heaterTargetTemperatureMax;
@@ -295,9 +256,7 @@ void loop() {
   //обработка нажатия кнопки Pattern
   if(buttonPattern.update()) {
     if(buttonPattern.read() == 0) {//кнопка нажата
- //     Serial.println("Button Pattern  pressed");
     } else { //кнопка отпущена
-  //    Serial.println("Button Pattern released");
       systemData.patternPresetNo = (systemData.patternPresetNo + 1) % arraySize(pwmPatternPreset); //меняем пресет
       systemData.patternFrequency = pwmPatternPreset[systemData.patternPresetNo].frequency;
       systemData.patternDutyPercent = pwmPatternPreset[systemData.patternPresetNo].dutyPercent;
@@ -305,9 +264,6 @@ void loop() {
         timeLaserPeriod = 1000 / systemData.patternFrequency ; //период работы лазера пунктиром в мс
         timeLaserWork = timeLaserPeriod * systemData.patternDutyPercent / 100;//работа пунктиром в мс
         timeLaserStop = timeLaserPeriod - timeLaserWork; //простой пунктиром в мс
-   //     Serial.println(timeLaserPeriod);
-  //      Serial.println(timeLaserWork);
-  //      Serial.println(timeLaserStop);
         Timer1.initialize(timeLaserWork*1000); // инициализировать timer1, и установить период в мкс
         Timer1.attachInterrupt(callback);  // прикрепить callback(), как обработчик прерывания по переполнению таймера
       } else {
